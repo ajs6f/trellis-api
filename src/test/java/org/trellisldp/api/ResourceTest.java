e6fc7035fa28fee2fbf4358318ae7b2e7b15aebb
Aaron Coburn <acoburn@amherst.edu>
2017-10-03 13:27:28 -0400

    Merge SPI interfaces into a single API package

--- src/test/java/org/trellisldp/api/ResourceTest.java
+++ src/test/java/org/trellisldp/api/ResourceTest.java
@@ -15,12 +15,14 @@ package org.trellisldp.api;
 
 import static java.util.Collections.singleton;
 import static java.util.stream.Stream.empty;
+import static java.util.stream.Stream.of;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyCollection;
 import static org.mockito.Mockito.doCallRealMethod;
 import static org.mockito.Mockito.when;
+import static org.trellisldp.vocabulary.Trellis.PreferUserManaged;
 
 import org.apache.commons.rdf.api.IRI;
 import org.apache.commons.rdf.api.RDF;
@@ -31,6 +33,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.trellisldp.vocabulary.DC;
 
 /**
  * @author acoburn
@@ -66,6 +69,26 @@ public class ResourceTest {
     public void testResource() {
         assertEquals(0L, mockResource.stream(prefer).count());
         assertEquals(0L, mockResource.stream(singleton(prefer)).count());
+        assertFalse(mockResource.getMembershipResource().isPresent());
+        assertFalse(mockResource.getMemberRelation().isPresent());
+        assertFalse(mockResource.getMemberOfRelation().isPresent());
+        assertFalse(mockResource.getInsertedContentRelation().isPresent());
+        assertFalse(mockResource.getMemberRelation().isPresent());
+        assertFalse(mockResource.getBinary().isPresent());
+        assertFalse(mockResource.isMemento());
+        assertFalse(mockResource.getInbox().isPresent());
+        assertFalse(mockResource.getAnnotationService().isPresent());
+    }
+
+    @Test
+    public void testResource2() {
+        final IRI subject = rdf.createIRI("ex:subject");
+        when(mockResource.stream()).thenAnswer((x) -> of(
+                    rdf.createQuad(prefer, subject, DC.title, rdf.createLiteral("A title")),
+                    rdf.createQuad(PreferUserManaged, subject, DC.title, rdf.createLiteral("Other title"))));
+
+        assertEquals(1L, mockResource.stream(prefer).count());
+        assertEquals(1L, mockResource.stream(singleton(prefer)).count());
         assertFalse(mockResource.getMembershipResource().isPresent());
         assertFalse(mockResource.getMemberRelation().isPresent());
         assertFalse(mockResource.getMemberOfRelation().isPresent());
